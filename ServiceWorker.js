ffbe65d4be02b2f0e7cff7217baa1a16d22fa195
anhhuynhnhut <huynhnhutanh2000@gmail.com>
2023-12-13 09:04:19 +0700

    up

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-SchoolTour-0.1";
 const contentToCache = [
     "Build/shcooltour.loader.js",
-    "Build/5d5da081feb1ad93a81367a2019ef65f.js",
-    "Build/ba87e0a7a13679afd4da4651ae670dbf.data",
-    "Build/06ed8d8c8216ed7aa18b4cc4ef97ac5b.wasm",
+    "Build/244106bdbae6a0cbead1e5b74e9f9507.js",
+    "Build/18b411494cf7a182ff54611297ee367c.data",
+    "Build/6413f85bd56ca103b69cb4c20c5a580e.wasm",
     "TemplateData/style.css"
 
 ];
